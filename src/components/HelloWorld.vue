2aff1fca94a03ad6417f12b9eb116a57caee571a
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-20 08:53:01 +0100

    add qr reader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,57 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { QrcodeStream } from 'vue-qrcode-reader';
+
+const torchAccess = ref(false);
+const torch = ref(false);
+
+const onDetect = (...a) => {
+  console.log(a);
+};
+
+const onInit = (capabilities) => {
+  torchAccess.value = !!capabilities.torch;
+};
+
+const paintOutline = (detectedCodes, ctx) => {
+  for (const detectedCode of detectedCodes) {
+    const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+    ctx.strokeStyle = 'red'
+
+    ctx.beginPath()
+    ctx.moveTo(firstPoint.x, firstPoint.y)
+    for (const { x, y } of otherPoints) {
+      ctx.lineTo(x, y)
+    }
+    ctx.lineTo(firstPoint.x, firstPoint.y)
+    ctx.closePath()
+    ctx.stroke()
+  }
+};
+
 </script>
 
 <template>
-  qr code
+  {{ torchAccess }}{{ torch }}
+  <button @click="torch=!torch">switch torch</button>
+  <qrcode-stream
+    @detect="onDetect"
+    @camera-on="onInit"
+    :track="paintOutline"
+    :constraints="{ facingMode: 'environment' }"
+    :torch="torch"
+    >
+    <div class="camera-overlay">
+    </div>
+  </qrcode-stream>
 </template>
 
 <style scoped>
+.camera-overlay {
+  text-align: right;
+  padding-top: 26px;
+  padding-right: 20px;
+/*  background: rgba(0, 100, 230, 30);*/
+}
 </style>
