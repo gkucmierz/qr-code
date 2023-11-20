69144b7de8eb433eb2b5bd55c4b0448d338fa1fa
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-20 11:31:41 +0100

    add torch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,9 +4,10 @@ import { QrcodeStream } from 'vue-qrcode-reader';
 
 const torchAccess = ref(false);
 const torch = ref(false);
+const detected = ref();
 
-const onDetect = (...a) => {
-  console.log(a);
+const onDetect = data => {
+  detected.value = data;
 };
 
 const onInit = (capabilities) => {
@@ -16,9 +17,7 @@ const onInit = (capabilities) => {
 const paintOutline = (detectedCodes, ctx) => {
   for (const detectedCode of detectedCodes) {
     const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
-
-    ctx.strokeStyle = 'red'
-
+    ctx.strokeStyle = 'red';
     ctx.beginPath()
     ctx.moveTo(firstPoint.x, firstPoint.y)
     for (const { x, y } of otherPoints) {
@@ -33,8 +32,6 @@ const paintOutline = (detectedCodes, ctx) => {
 </script>
 
 <template>
-  {{ torchAccess }}{{ torch }}
-  <button @click="torch=!torch">switch torch</button>
   <qrcode-stream
     @detect="onDetect"
     @camera-on="onInit"
@@ -43,15 +40,28 @@ const paintOutline = (detectedCodes, ctx) => {
     :torch="torch"
     >
     <div class="camera-overlay">
+      <span v-if="torchAccess" class="pi pi-sun torch-icon" :class="{on: torch}" @click="torch = !torch"></span>
     </div>
   </qrcode-stream>
+  <pre>
+    {{ JSON.stringify(detected, null, '  ') }}
+  </pre>
 </template>
 
 <style scoped>
 .camera-overlay {
   text-align: right;
-  padding-top: 26px;
-  padding-right: 20px;
-/*  background: rgba(0, 100, 230, 30);*/
+  height: 100%;
+/*  background: rgba(0, 100, 230, 0.3);*/
+}
+.torch-icon {
+  margin: 20px 16px;
+  cursor: pointer;
+  font-size: 2rem;
+  color: #ccc;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+}
+.torch-icon.on {
+  color: #fff;
 }
 </style>
