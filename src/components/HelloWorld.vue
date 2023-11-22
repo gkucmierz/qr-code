78c580f0f8e62b5017020b89d39eb5dc6ac0be08
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-22 19:13:32 +0100

    fix outline freeze

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,21 +5,18 @@ import { QrcodeStream } from 'vue-qrcode-reader';
 const torchAccess = ref(false);
 const torch = ref(false);
 const detected = ref();
-const facingMode = ref('environment');
 const error = ref('');
 const paused = ref(false);
+const constraints = ref({
+  facingMode: 'environment',
+});
 
 const onDetect = data => {
-  // setTimeout(() => {
-    detected.value = data;
-  // }, 1000);
-  // console.log(data);
-  // paused.value = true;
-  // setTimeout(() => paused.value = false, 0);
+  detected.value = data;
 };
 
 const onInit = (capabilities) => {
-  torchAccess.value = true;//!!capabilities.torch;
+  torchAccess.value = !!capabilities.torch;
 };
 
 const paintOutline = (detectedCodes, ctx) => {
@@ -39,7 +36,7 @@ const paintOutline = (detectedCodes, ctx) => {
 
 const switchCamera = () => {
   const modes = ['user', 'environment'];
-  facingMode.value = modes[(modes.indexOf(facingMode.value) + 1) % modes.length];
+  constraints.facingMode.value = modes[(modes.indexOf(constraints.facingMode.value) + 1) % modes.length];
 };
 
 const onError = (err) => {
@@ -56,11 +53,11 @@ const onError = (err) => {
       @error="onError"
       :paused="paused"
       :track="paintOutline"
-      :constraints="{ facingMode: facingMode }"
+      :constraints="constraints"
       :torch="torch"
       >
       <div class="camera-overlay">
-        <span class="pi pi-camera camera-icon" @click="switchCamera"></span>
+        <span class="pi pi-camera camera-icon" @click="switchCamera()"></span>
         <span v-if="torchAccess" class="pi pi-sun torch-icon" :class="{on: torch}" @click="torch = !torch"></span>
       </div>
     </qrcode-stream>
@@ -77,7 +74,6 @@ const onError = (err) => {
 .camera-overlay {
   display: flex;
   justify-content: space-between;
-  height: 100%;
 /*  background: rgba(0, 100, 230, 0.3);*/
 }
 .torch-icon {
